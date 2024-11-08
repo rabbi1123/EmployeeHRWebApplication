944e93a85bebccb5d56c1f9573fa67d77d135a30
rabbi1123 <mushfikur80@gmail.com>
2024-11-09 00:28:09 +0600

    Add project files.

---
+++ WebUI/HRApplication.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HRApplication.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
